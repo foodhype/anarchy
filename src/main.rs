95b700c2e30ac4116ca302d077ef3a33f1bfce0a
foodhype <brianschmitz.main@gmail.com>
2014-10-18 16:55:41 -0500

    playing around with Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!")
+use std::io::Timer;
+use std::time::Duration;
+use std::io::{TcpListener, TcpStream};
+use std::io::{Acceptor, Listener};
+
+
+fn main() {  
+    spawn(proc() {
+        let listener = TcpListener::bind("127.0.0.1", 8001);
+
+        // bind the listener to the specified address
+        let mut acceptor = listener.listen();
+
+        // accept connections and process them, spawning a new tasks for each one
+        for stream in acceptor.incoming() {
+            match stream {
+                Err(e) => {
+                    println!("{}", e);
+                }
+                Ok(stream) => spawn(proc() {
+                })
+            }
+        }
+
+        // close the socket server
+        drop(acceptor);
+    });
+
+    spawn(proc() {
+        let mut timer = Timer::new().unwrap();
+        timer.sleep(Duration::milliseconds(400));
+        let mut socket = TcpStream::connect("127.0.0.1", 8001);
+        loop {
+            let response = socket.read_to_end();
+            println!("{}", response);
+            timer.sleep(Duration::milliseconds(300));
+        }
+    });
+
 }
